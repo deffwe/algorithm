1ceeeebcbafce14708e7e0c83c1d728e72820ef9
deffwe <deffwe@gmail.com>
2013-10-27 17:11:42 +0800

    字符串包含完结，kmp算法和计数排序字符串(c++)

--- 字符串包含/main.cpp
+++ 字符串包含/main.cpp
@@ -1,50 +1,25 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <map>
-#include <set>
-#include <algorithm>
-#include "count_sort.h"
-#include "kmp.h"
 using namespace std;
-extern string::size_type kmp(string);
-extern string count_sort(string);
 int main() {
-    string s1, s2;
-    while(getline(cin, s1)) {
-        getline(cin, s2);
-		s1 = count_sort(s1);
-        s2 = count_sort(s2);
-		map<char, string::size_type> kmpv;
-		for(string::size_type i = 0; i != s2.size(); ++i) {
-			string cs = s2.substr(0, i + 1);
-			kmpv.insert(make_pair(s2[i], kmp(cs)));
-		}	
-		string::size_type i = 0, j = 0, k = 0;
-		bool contain = false;
-		while(i < s1.size()) {
-			if(s1[i] == s2[j]) {
-				++i;
-				++j;
-				++k;
-				if(k == s2.size()) {
-					contain = true;
-					break;
-				}
-			} else {
-				if(k == 0) {
-					++i; 
-					j = 0;
-				}
-				else {
-					i += k - kmpv[s2[j-1]];
-					k = j = 0;		
-				}
+	string s1, s2;
+	while(getline(cin, s1)) {
+		getline(cin, s2);
+		map<char, string::size_type> hmap;
+		for(auto c:s1) {
+			hmap.insert(make_pair(c, 1));
+		}
+		bool contain = true;
+		for(auto c:s2){
+			auto insert_result = hmap.insert(make_pair(c, 1));
+			if(insert_result.second) {
+				contain = false;
 			}
 		}
+
 		if(contain) cout << "包含" << endl;
 		else cout << "不包含" << endl;
-    }
+	}
 
-    return 0;
+	return 0;
 }
